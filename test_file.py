98034511499f8aa275d6f8a2061dc2a94d90dac0
Christopher <romanjaimesc@gmail.com>
2022-07-10 20:57:06 -0500

    Add prod_list function

--- test_file.py
+++ test_file.py
@@ -4,4 +4,7 @@ def get_uniques(list_test: list) -> np.array:
     return np.unique(list_test)
 
 def sum_list(list_test: list) -> float:
-    return np.sum(sum_list)
+    return np.sum(list_test)
+
+def prod_list(list_test: list) -> float:
+    return np.sum(list_test)
