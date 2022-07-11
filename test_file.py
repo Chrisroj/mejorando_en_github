0ac74c44c1d6a3ae2637ec94ee32807c2a8f981d
Christopher <romanjaimesc@gmail.com>
2022-07-10 20:47:52 -0500

    First Commit in a new branch

--- test_file.py
+++ test_file.py
@@ -1,4 +1,7 @@
 import numpy as np
 
-def get_uniques(list_test: list)->np.array:
+def get_uniques(list_test: list) -> np.array:
     return np.unique(list_test)
+
+def sum_list(list_test: list) -> float:
+    return np.sum(sum_list)
